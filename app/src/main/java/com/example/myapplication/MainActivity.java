bc1aa61f8c60b9203ff84e9d54d412906ccbf418
yuvrajsingh1010 <yuvrajsinghero@gmail.com>
2023-05-18 07:57:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.thumb)
+                .setTitle("Be Alert!")
+                .setMessage("Social Distancing")
+                .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_LONG).show();
+                    }
+                }).show();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
